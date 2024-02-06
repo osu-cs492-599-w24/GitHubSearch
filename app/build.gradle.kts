a958cd4782bec89c88051c14cd23f90603570591
Rob Hess <hessro@oregonstate.edu>
2024-02-06 15:34:19 -0800

    Add code from 2/6 lecture.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
